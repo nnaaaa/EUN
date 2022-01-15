cdcb8670c855286fb919998c6f3d440d80403579
NNAAAA <nngguyen.anh@gmail.com>
2022-01-15 08:07:23 +0700

    change server url

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,5 +1,5 @@
-export const SERVER_EXPRESS = process.env.SERVER_EXPRESS || 'http://localhost:5000'
-export const SERVER_SOCKET = process.env.SERVER_SOCKET || 'http://localhost:9000'
+export const SERVER_EXPRESS = process.env.SERVER_EXPRESS || 'https://facebookserverexpress.herokuapp.com/'
+export const SERVER_SOCKET = process.env.SERVER_SOCKET || 'https://facebookserversocket.herokuapp.com/'
 
 interface IDB {
     name: string
